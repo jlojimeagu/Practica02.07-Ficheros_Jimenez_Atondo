097c5a565c45b49303332728f3e38264e7dca37a
juan miguel <jlojimeagu1@educacion.navarra.es>
2022-12-19 10:43:25 +0100

    ejercicio3 completado

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,4 +1,4 @@
-def tabla_multiplicar():
+def tabla_multiplicar(n,m):
     nombre_fichero = 'tabla-' + str(n) + '.txt'
     try:
         with open(nombre_fichero, 'r') as file:
@@ -7,6 +7,8 @@ def tabla_multiplicar():
     except FileNotFoundError:
         print('No existe el fichero con la tabla del ', n)
 
-tabla_multiplicar()
+
 n = int(input('Introduce un número entero entre 1 y 10: '))
 m = int(input('Introduce otro número entero entre 1 y 10: '))
+
+print(tabla_multiplicar(n,m))
